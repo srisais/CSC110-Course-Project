8902fc557da76752c7157ab1596bb6e993039103
srisais <electronic-mail>
2021-11-17 01:31:32 -0500

    Overhauled dataclasses--made them proper classes and completed more functions

--- Program Files/data_types.py
+++ Program Files/data_types.py
@@ -9,87 +9,127 @@ enabling the data to be easily manipulate and analyzed.
 
 NOTE: "enrollment" vs "enrolment"
       I will be spelling the word "enrollment" (with 2 'l's).
+
+TODO: add __str__ magic methods to the classes so debugging is much easier
+      (have __str__ show all the object attributes or something)
+TODO: Fix line lengths and other PEP8 formatting issues
 """
-from dataclasses import dataclass
 
-@dataclass
+
 class Course:
-    """A data type that represents the data for
-    an Ontario Secondary School Course
+    """This class regarding the datatype that represents the data
+    for an Ontario Secondary School Course
 
-    >>> # Example Course object
-    >>> c = Course(code="TKQ4T",\
-                   title="Healthy Cooking Made Easy",\
-                   grade_level=12,\
-                   pathway_or_destination="College Delivered (Dual Credits)")
+    This class includes relevant methods for using Course objects.
     """
-    code: str
-    title: str  # sometimes referred to course_description
-    grade_or_level: str  # Ex: "Grade 9" or "Level 2"
-    pathway_or_destination: str
+    def __init__(self, code: str, title: str, grade_or_level: str, pathway_or_destination: str) -> None:
+        """Creates a Course object
+
+        >>> # Example Course object
+        >>> c = Course(code="TKQ4T",\
+                       title="Healthy Cooking Made Easy",\
+                       grade_or_level="Grade 12",\
+                       pathway_or_destination="College Delivered (Dual Credits)")
+        """
+
+        self.code = code
+        self.title = title  # sometimes referred to course_description
+        self.grade_or_level = grade_or_level  # Ex: "Grade 9" or "Level 2"
+        self.pathway_or_destination = pathway_or_destination
 
 
-@dataclass
 class CourseEnrollment:
-    """A data type that represents the data for
+    """A class regarding the data type that represents the data for
     a specific Course object and the number of students enrolled
-    in that course
+    in that course for a specific school year.
 
-    >>> c = Course(code="TKQ4T",\
-                   title="Healthy Cooking Made Easy",\
-                   grade_level=12,\
-                   pathway_or_destination="College Delivered (Dual Credits)")
-    >>> # Example CourseEnrollment object
-    >>> d = CourseEnrollment(course=c, num_enrollments=11)
+    This class includes relevant methods for using CourseEnrollment objects.
     """
-    course: Course
-    num_enrollments: int
-    # IMPORTANT NOTE ABOUT num_enrollments:
-    # Since the dataset shows "<10" as the smallest values,
-    # any values that show "<10" will be represented by 9.
+    def __init__(self, course: Course, num_enrollments: int, start_year: int, end_year: int) -> None:
+        """Creates a CourseEnrollment object.
+
+        >>> c = Course(code="TKQ4T",\
+                       title="Healthy Cooking Made Easy",\
+                       grade_or_level="Grade 12",\
+                       pathway_or_destination="College Delivered (Dual Credits)",)
+        >>> # Example CourseEnrollment object
+        >>> d = CourseEnrollment(course=c, num_enrollments=11, start_year=2011, end_year=2012)
+        """
+        self.course = course
+        self.num_enrollments = num_enrollments
+        # IMPORTANT NOTE ABOUT num_enrollments:
+        # Since the dataset shows "<10" as the smallest values,
+        # any values that show "<10" will be represented by 9.
+
+        self.start_year = start_year
+        self.end_year = end_year
+        self.school_year_str = f"{self.start_year} - {self.end_year}"
 
 
-@dataclass
-class SchoolYearCourseEnrollments:
-    """A data type that represents a specific school year
+class SchoolYearAllCourseEnrollments:
+    """A class regarding the data type that represents a specific school year
     and all of the courses offered in that school year
     with their enrollment numbers
 
-    >>> c1 = Course(code="TKQ4T",\
-                   title="Healthy Cooking Made Easy",\
-                   grade_level=12,\
-                   pathway_or_destination="College Delivered (Dual Credits)")
-    >>> d1 = CourseEnrollment(course=c1, num_enrollments=11)
-    >>> c2 = Course(code="MCV4U",\
-                   title="Calculus and Vectors",\
-                   grade_level=12,\
-                   pathway_or_destination=\
-                   "University Preparation (Grade 11 & 12 level)")
-    >>> d2 = CourseEnrollment(course=c2, num_enrollments=38630)
-    >>> # Example SchoolYearCourseEnrollments object
-    >>> s = SchoolYearCourseEnrollments(start_year=2012,\
-                              end_year=2013,\
-                              list_of_courses=[d1, d2])
-    >>> # This is example list for list_of_courses with only 2 elements.
-    >>> # A typical list of list_of_courses will have ~1500 elements.
+    This class includes relevant methods for using SchoolYearAllCourseEnrollments objects.
     """
-    start_year: int
-    end_year: int
-    list_of_course_enrollments: list[CourseEnrollment]
+    def __init__(self, start_year: int, end_year: int, list_of_course_enrollments: list[CourseEnrollment]) -> None:
+        """Creates a SchoolYearCourseEnrollments object
+
+        >>> c1 = Course(code="TKQ4T",\
+                       title="Healthy Cooking Made Easy",\
+                       grade_or_level="Grade 12",\
+                       pathway_or_destination="College Delivered (Dual Credits)")
+        >>> d1 = CourseEnrollment(course=c1, num_enrollments=11, start_year=2011, end_year=2012)
+        >>> c2 = Course(code="MCV4U",\
+                       title="Calculus and Vectors",\
+                       grade_or_level="Grade 12",\
+                       pathway_or_destination=\
+                       "University Preparation (Grade 11 & 12 level)")
+        >>> d2 = CourseEnrollment(course=c2, num_enrollments=38630, start_year=2011, end_year=2012)
+        >>> # Example SchoolYearCourseEnrollments object
+        >>> s = SchoolYearAllCourseEnrollments(start_year=2012,\
+                                  end_year=2013,\
+                                  list_of_course_enrollments=[d1, d2])
+        >>> # This is example list for list_of_courses with only 2 elements.
+        >>> # A typical list of list_of_courses will have ~1500 elements.
+        """
+        self.start_year = start_year
+        self.end_year = end_year
+        self.school_year_str = f"{self.start_year} - {self.end_year}"
+        self.list_of_course_enrollments = list_of_course_enrollments
+
+
+class CourseEnrollmentHistory:
+    """A class regarding the data type that represents the same course and
+    its enrolment data over a number of school years
+
+    This class includes methods for utilizing CourseEnrollmentHistory objects.
+    """
+    def __init__(self, course: Course, course_enrollment_list: list[CourseEnrollment]) -> None:
+        """Creates a CourseEnrollmentHistory object
+
+        Preconditions:
+            - None of the CourseEnrollment objects in the list have the same school year
+            - All of the CourseEnrollment objects in the list are of the same course
+
+        >>> c = Course(code="MCV4U",\
+                       title="Calculus and Vectors",\
+                       grade_or_level="Grade 12",\
+                       pathway_or_destination=\
+                       "University Preparation (Grade 11 & 12 level)")
+        >>> d1 = CourseEnrollment(course=c, num_enrollments=38630, start_year=2012, end_year=2013)
+        >>> d2 = CourseEnrollment(course=c, num_enrollments=38630, start_year=2011, end_year=2012)
+        >>> # Example CourseEnrollmentHistory object
+        >>> ceh = CourseEnrollmentHistory(c, [d1, d2])
+        >>> # This list only has 2 CourseEnrollment objects;
+        >>> # Typically, this list would have ~10 objects.
+        """
+        self.course = course
+        self.year_str_to_course_enrollment = {
+            course_enrollment.school_year_str: course_enrollment
+            for course_enrollment in course_enrollment_list
+        }
 
 
 
-@dataclass
-class CourseEnrollment over the years?... {a better name}:
-    """A data type """
-
-
-# some data type that kinda is like this dict:
-"""
-
-{   <year>: <CourseEnrollment object>,
-    ...
-}
-
-
-"""
--- Program Files/functions.py
+++ Program Files/functions.py
@@ -8,9 +8,12 @@ This file contains the functions required to parse database files and assist
 in converting them into usable data. These functions will
 import, sanitize, and sort the data in accordance with how the
 dataclasses and other required structures are defined.
+
+TODO: Fix line lengths and other PEP8 formatting issues
 """
 
 from data_types import *
+
 
 ###############################################################################
 # Importing .txt files
@@ -24,12 +27,10 @@ def get_text_file(file_path: str) -> list[str]:
         - file_path is a global file path to a .txt file that can be accessed
         - file_path[-4:] == ".txt"
     """
-
     file_lines = []
 
     with open(file_path, 'r') as cur_file:
         for cur_line in cur_file:
-            # breakpoint()
             file_lines.append(cur_line)
 
     return file_lines
@@ -52,7 +53,7 @@ def split_line_into_list(line: str, delimiter: str="|") -> list[str]:
 
 
 def format_line_list_elements(line_list: list[str]) -> \
-        list[str, str, int, str, int]:
+        list[str, str, str, str, int]:
     """Converts the elements of a list -- formatted like to the output
     of split_line_into_list -- to the required elements.
     (Does not mutate the original list)
@@ -84,6 +85,8 @@ def format_line_list_elements(line_list: list[str]) -> \
     else:
         num_enrolments = int(num_enrolments)
 
+    n_en = int(num_enrolments)
+
     # IMPORTANT NOTE ABOUT num_enrollments:
     # Since the dataset shows "<10" as the smallest values,
     # any values that show "<10" will be represented by 9.
@@ -95,44 +98,128 @@ def format_line_list_elements(line_list: list[str]) -> \
         line_list[1],
         line_list[2],
         line_list[3],
-        num_enrolments
+        n_en
     ]
 
 
-###############################################################################
-# Creating dataclass Objects
-###############################################################################
+def get_school_year_from_file_path(file_path: str) -> tuple[int, int]:
+    """Extracts and returns a tuple of the start and end year of the
+    current text file using the file path
 
-# Theses might be better as __init__ methods in their respective dataclasses...
+    Preconditions:
+    - The file name is preceeded either by '/' or '\'
+    - The file title beginning follows the following format:
+        f"mdc_enrol_{XXYY}"
 
-def create_course_object(code: str, title: str, grade_or_level: str,\
-                         pathway_or_destination: str) -> Course:
-    """Creates and returns a Course object."""
-    return Course(
-        code=code,
-        title=title,
-        grade_or_level=grade_or_level,
-        pathway_or_destination=pathway_or_destination
+    >>> get_school_year_from_file_path("mdc_enrol_1314_en_supp_2")
+    (2013, 2014)
+    >>> path_1 = "C:\\Bob\\mdc_enrol_1516_en_supp.txt"
+    >>> get_school_year_from_file_path(path_1)
+    (2015, 2016)
+    >>> path_2 = "/System/Potato/mdc_enrol_1819_en_supp.txt"
+    >>> get_school_year_from_file_path(path_2)
+    (2018, 2019)
+    """
+    # Example file title: "mdc_enrol_1314_en_supp_2"
+    text_file_title = ""
+    for i in range(-1, -len(file_path) - 1, -1):  # Goes from last to first char
+        if file_path[i] == '\\' or file_path[i] == '/':
+            # Remember, i is a negative index
+            text_file_title = file_path[i + 1:]
+            break
+    else:
+        text_file_title = file_path
+
+    default_century = 2000
+    year_nums = text_file_title[10:14]  # Ex: "1213"
+    start_year = default_century + int(year_nums[0:2])
+    end_year = default_century + int(year_nums[2:])
+
+    return (start_year, end_year)
+
+
+def get_school_year_all_course_enrollments(file_path: str) -> SchoolYearAllCourseEnrollments:
+    """Gets the data from a text file (part of the database)
+    and formats it and returns a SchoolYearAllCourseEnrollments
+    for that text file's respective year
+
+    TODO: Add doctests for this function
+    >>>
+    """
+
+    text_file_lines = get_text_file(file_path)[1:]  # Excluding the first line (of table headers)
+    text_file_list = []
+
+    for line in text_file_lines:
+        text_file_list.append(
+            format_line_list_elements(
+                split_line_into_list(line)
+            )
+        )
+
+    start_year, end_year = get_school_year_from_file_path(file_path)
+
+    course_enrollment_list = []
+
+    for course in text_file_list:
+        cur_course = Course(
+            *course[0:4]  # The * unpacks the list
+        )
+
+        cur_course_enrollment = CourseEnrollment(
+            cur_course, course[4], start_year, end_year  # I'm not sure why Pycharm thinks course[4] is a str, rather than an int
+        )
+
+        course_enrollment_list.append(
+            cur_course_enrollment
+        )
+
+    return SchoolYearAllCourseEnrollments(
+        start_year,
+        end_year,
+        course_enrollment_list
     )
 
 
-def create_course_enrollment_object(course: Course, num_enrollments: int)\
-    -> CourseEnrollment:
-    """Creates and returns a CourseEnrollment object."""
-    return CourseEnrollment(
-        course=course,
-        num_enrollments=num_enrollments
-    )
+def get_course_enrollment_history_for_all_courses(list_of_file_paths: list[str]) -> list[CourseEnrollmentHistory]:
+    """Returns a massive list of CourseEnrollmentHistory objects, one per course
+
+    TODO: test this function? or add doctests... ? see if it works properly, somehow
+    """
+
+    course_enrollment_history_for_all_courses_list = []
+    course_enrollment_history_for_all_courses_dict = {}
+    school_year_all_course_enrollments_list = []
+
+    for file_path in list_of_file_paths:
+        school_year_all_course_enrollments_list.append(
+            get_school_year_all_course_enrollments(file_path)
+        )
+
+    # Fills up the dict with the keys being Course objects
+    # and the values being lists of CourseEnrollment objects
+    for school_year in school_year_all_course_enrollments_list:
+        for course_enrollment in school_year.list_of_course_enrollments:
+            if course_enrollment.course not in course_enrollment_history_for_all_courses_dict:
+                course_enrollment_history_for_all_courses_dict[course_enrollment.course] = []
+            course_enrollment_history_for_all_courses_dict[course_enrollment.course].append(
+                course_enrollment
+            )
+
+    for course, course_enrollment_history in course_enrollment_history_for_all_courses_dict.items():
+        course_enrollment_history_for_all_courses_list.append(
+            CourseEnrollmentHistory(
+                course,
+                course_enrollment_history
+            )
+        )
+
+    return course_enrollment_history_for_all_courses_list
 
 
-def create_school_year_enrollments_object(start_year: int, end_year: int,
-                                          list_of_course_enrollments:\
-                                                  list[CourseEnrollment])\
-        -> SchoolYearCourseEnrollments:
-    """Creates and returns a SchooYearCourseEnrollment object."""
-    return SchoolYearCourseEnrollments(
-        start_year=start_year,
-        end_year=end_year,
-        list_of_course_enrollments=list_of_course_enrollments
-    )
+
+
+
+
+
 
